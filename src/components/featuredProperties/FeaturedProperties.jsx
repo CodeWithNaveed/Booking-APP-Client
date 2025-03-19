3171e7cf03746aa9acb49a00e7ecbe3f7b81dccb
naveed ali abbasi <getinfo.naveed@gmail.com>
2025-03-19 11:32:08 +0500

    set

--- src/components/featuredProperties/FeaturedProperties.jsx
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -1,33 +1,32 @@
 import useFetch from "../../hooks/useFetch";
 import "./featuredProperties.css";
+import hotelImage from "../images/hotel.jpeg";
 
 const FeaturedProperties = () => {
   const { data, loading, error } = useFetch("/hotels?featured=true&limit=4");
 
-  console.log("Fetched Data:", data); // Debugging line
-
   return (
     <div className="fp">
       {loading ? (
-        "Loading..."
-      ) : error ? (
-        <p>Error fetching data</p>
-      ) : !Array.isArray(data) ? ( // Check if data is an array
-        <p>No properties available</p>
+        "Loading"
       ) : (
         <>
           {data.map((item) => (
             <div className="fpItem" key={item._id}>
-              <img src={item.photos?.[0]} alt="Featured" className="fpImg" />
+              <img
+                src={item.photos?.[0] || hotelImage}
+                alt="Featured"
+                className="fpImg"
+              />
               <span className="fpName">{item.name}</span>
               <span className="fpCity">{item.city}</span>
               <span className="fpPrice">Starting from ${item.cheapestPrice}</span>
-              {item.cheapestPrice && (
+              {item.cheapestPrice &&
                 <div className="fpRating">
                   <button>{item.cheapestPrice}</button>
                   <span>Excellent</span>
                 </div>
-              )}
+              }
             </div>
           ))}
         </>
