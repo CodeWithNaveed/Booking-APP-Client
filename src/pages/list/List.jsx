7aee669ab4829102e96eb3b5b1219289f3f7465f
naveed ali abbasi <getinfo.naveed@gmail.com>
2025-03-19 19:33:42 +0500

    set

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -30,7 +30,7 @@ const List = () => {
   const [max, setMax] = useState(undefined);
 
   const { data, loading, error, reFetch } = useFetch(
-    `/hotels?city=${destination}&min=${min || 0}&max=${max || 999}`
+    `https://booking-app-api-production-8253.up.railway.app/api/hotels?city=${destination}&min=${min || 0}&max=${max || 999}`
   );
 
   const handleClick = () => {
