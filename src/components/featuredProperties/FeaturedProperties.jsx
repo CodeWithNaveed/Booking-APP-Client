6b7e1fe9b287619eb6a904b612a8dd76a6a69069
naveed ali abbasi <getinfo.naveed@gmail.com>
2025-03-19 11:01:59 +0500

    set url in component

--- src/components/featuredProperties/FeaturedProperties.jsx
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -13,7 +13,7 @@ const FeaturedProperties = () => {
           {data.map((item) => (
             <div className="fpItem" key={item._id}>
               <img
-                src={item.photos[0] || "https://cf.bstatic.com/xdata/images/hotel/max1024x768/261707778.jpg?k=96f39f6d85c802b97f689c84c5fe4d3978bf3f13b0d70d6f795f0e6c8790f9da&o="}
+                src={item.photos[0]}
                 alt="Featured"
                 className="fpImg"
               />
